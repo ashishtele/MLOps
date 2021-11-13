b98b62779cfdef35ecccc3faa527a496c933a2d4
Ashish Tele <sale.ashish.tele@gmail.com>
2021-11-13 14:30:02 -0500

    adding tempalte

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps Toy Example End to End',
+    author='Ashish_Tele',
+    license='MIT',
+)
