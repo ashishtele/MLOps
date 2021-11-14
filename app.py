4cad3c0a0c559ac5c270c0d4edc959b2b341d640
Ashish Tele <sale.ashish.tele@gmail.com>
2021-11-14 15:42:03 -0500

    Requirement file updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
 import yaml
