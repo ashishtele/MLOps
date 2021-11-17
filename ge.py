4c4b2af4a21c7a437a19c6d90f1d4b6348c2a42b
Ashish Tele <sale.ashish.tele@gmail.com>
2021-11-16 19:55:07 -0500

    Great Exp is updated

---
+++ ge.py
@@ -0,0 +1,31 @@
+import great_expectations as ge
+
+context = ge.data_context.DataContext()
+
+suite = context.create_expectation_suite(
+    expectation_suite_name="check_raw_data",
+    overwrite_existing = True
+)
+
+batch_kwargs = {
+    'path': 'data/raw/train.csv',
+    'datasource': 'data_dir',
+    'data_asset_name': 'train',
+    'reader_method': 'read_csv'
+}
+batch = context.get_batch(batch_kwargs, suite)
+print(batch.head())
+
+# Expect column to exists
+batch.expect_column_to_exist('churn')
+batch.expect_column_values_to_not_be_null('churn')
+batch.expect_column_values_to_be_of_type('total_day_calls', 'int')
+batch.expect_column_distinct_values_to_be_in_set('churn', ['no', 'yes'])
+
+# print expectation_suite
+print(batch.get_expectation_suite())
+
+batch.save_expectation_suite()
+results = context.run_validation_operator('my_validation_operator', assets_to_validate=[batch])
+
+context.open_data_docs()
